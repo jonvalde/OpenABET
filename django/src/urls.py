ba5be1d592b1c68068552fc359640059db1bb728
jonvalde <valdesuso_82052@students.pupr.edu>
2019-05-24 22:26:38 -0400

    commit to update Diego

--- django/src/urls.py
+++ django/src/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('pages.urls')),
     path('universities/', include('universities.urls')),
+    path('campuses/', include('campuses.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
